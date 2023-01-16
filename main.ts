c7bbb8d1e86dd7e5ac290aab209227ef30372dd1
rkdtldn <105910264+rkdtldn@users.noreply.github.com>
2023-01-16 12:08:29 +0900

    pxt.json, main.blocks, main.ts, main.py 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
 basic.forever(function () {
-	
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+    basic.pause(100)
 })
